9e57d8c789cc59262b775922668c5477b2e83079
ir4kgl <vishneviyshturman@gmail.com>
2021-04-20 11:51:04 +0300

    add Make files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ira_goloborodko/Desktop/Course work/project-2020
+# Build directory: /home/ira_goloborodko/Desktop/Course work/project-2020/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
