1c83c4ca44c88973a67a77c0fff226af4c29679d
ir4kgl <vishneviyshturman@gmail.com>
2021-04-26 08:47:00 +0300

    fix header files

--- tests/test_givens_rotator.h
+++ tests/test_givens_rotator.h
@@ -1,3 +1,5 @@
 namespace test_givens_rotator {
+bool simple_check_left();
+bool simple_check_right();
 void run();
 }  // namespace test_givens_rotator
--- tests/test_householder_reflector.h
+++ tests/test_householder_reflector.h
@@ -1,5 +1,5 @@
 namespace test_householder_reflector {
-
+bool simple_check_left();
+bool simple_check_right();
 void run();
-
 }  // namespace test_householder_reflector
