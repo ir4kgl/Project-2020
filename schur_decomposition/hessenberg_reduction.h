4db7c56e1f8b0ff75b42e1da88263fea1a785b00
ir4kgl <vishneviyshturman@gmail.com>
2021-04-26 09:08:48 +0300

    update reduction to Hessenberg form algorithm

--- schur_decomposition/hessenberg_reduction.h
+++ schur_decomposition/hessenberg_reduction.h
@@ -1,29 +1,29 @@
-#include "householder_reflection.h"
+#include "../eigen/Eigen/Dense"
+#include "../schur_decomposition/householder_reflection.h"
 
 namespace hessenberg_reduction {
 
+using householder_reflection::HouseholderReflector;
+
 template <typename Scalar>
-class Hessenberg_reduction {
+class HessenbergReduction {
   static_assert(std::is_arithmetic_v<Scalar>,
                 "Scalar must be arithmetic type!");
 
   using MatrixDynamic = Eigen::Matrix<Scalar, -1, -1>;
   using UnitaryMatrix = Eigen::Matrix<Scalar, -1, -1>;
-  using HouseholderReflector =
-      householder_reflection::Householder_reflector<Scalar>;
 
  public:
   void run(MatrixDynamic* data, UnitaryMatrix* backtrace) {
     assert(data.rows() == data.cols());
-    full_size_ = data->rows();
+    size_ = data->rows();
     p_hessenberg_form_ = data;
     p_backtrace_matrix_ = backtrace;
-    rows_cnt_ = full_size_;
 
-    for (col_ = 0; col_ < full_size_ - 2; ++col_) {
-      --rows_cnt_;
-      reflector_ =
-          Reflector(p_hessenberg_form_->col(col_).bottomRows(rows_cnt_));
+    for (col_ = 0; col_ < size_ - 2; ++col_) {
+      zeroed_counter_ = size_ - col_ - 1;
+      reflector_ = HouseholderReflector<Scalar>(
+          p_hessenberg_form_->col(col_).bottomRows(zeroed_counter_));
       reduce_column();
     }
     reflector_ = {};
@@ -32,20 +32,20 @@ class Hessenberg_reduction {
  private:
   void reduce_column() {
     reflector_.reflect_left(
-        p_hessenberg_form_->bottomRightCorner(rows_cnt_, full_size_ - col_));
+        p_hessenberg_form_->bottomRightCorner(zeroed_counter_, size_ - col_));
     reflector_.reflect_right(
-        p_hessenberg_form_->bottomRightCorner(full_size_, rows_cnt_));
+        p_hessenberg_form_->bottomRightCorner(size_, zeroed_counter_));
     reflector_.reflect_right(
-        p_backtrace_matrix_->bottomRightCorner(full_size_, rows_cnt_));
+        p_backtrace_matrix_->bottomRightCorner(size_, zeroed_counter_));
   }
 
   MatrixDynamic* p_hessenberg_form_;
   UnitaryMatrix* p_backtrace_matrix_;
-  HouseholderReflector reflector_;
+  HouseholderReflector<Scalar> reflector_;
 
-  int full_size_;
+  int size_;
   int col_;
-  int rows_cnt_;
+  int zeroed_counter_;
 
 };  // namespace hessenberg_reduction
 
