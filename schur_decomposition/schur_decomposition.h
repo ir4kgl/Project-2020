b6cdc6c36c5dcd988724f962700e0ece61f15291
ir4kgl <vishneviyshturman@gmail.com>
2021-05-04 21:47:17 +0300

    add SchurDecomposition members and constructor

--- schur_decomposition/schur_decomposition.h
+++ schur_decomposition/schur_decomposition.h
@@ -16,7 +16,38 @@ class SchurDecomposition {
   using SchurForm = Eigen::Matrix<Scalar, -1, -1>;
   using SquareMatrix = Eigen::Matrix<Scalar, -1, -1>;
   using UnitaryMatrix = Eigen::Matrix<Scalar, -1, -1>;
+  using Precision = Scalar;
 
-  void run(const SquareMatrix& data, SchurForm* out, UnitaryMatrix* unitary) {}
+ public:
+  SchurDecomposition(Precision precision) : precision_(precision) {
+    assert(precision >= 0);
+  }
+
+  void run(const SquareMatrix& data, SchurForm* schur_form,
+           UnitaryMatrix* unitary) {
+    assert(data.rows() == data.cols());
+    assert(schur_form);
+    assert(unitary);
+
+    data_size_ = data.rows();
+    p_schur_form_ = schur_form;
+    *p_schur_form_ = data;
+    p_unitary_ = unitary;
+    *p_unitary_ = UnitaryMatrix::Identity(data_size_, data_size_);
+  }
+
+  void set_precision(Precision precision) {
+    assert(precision >= 0);
+    precision_ = precision;
+  }
+
+  Precision get_precision() const { return precision_; }
+
+ private:
+  Precision precision_;
+  SchurForm* p_schur_form_;
+  UnitaryMatrix* p_unitary_;
+  int data_size_;
 };
+
 };  // namespace schur_decomposition
