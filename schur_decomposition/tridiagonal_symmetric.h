bfe543ff1233541db5634b8e099d13e593b44160
ir4kgl <vishneviyshturman@gmail.com>
2021-05-25 13:42:10 +0300

    update TridiagonalSymmetric

--- schur_decomposition/tridiagonal_symmetric.h
+++ schur_decomposition/tridiagonal_symmetric.h
@@ -8,15 +8,17 @@ class TridiagonalSymmetric {
   using DynamicMatrix = Eigen::Matrix<Scalar, -1, -1>;
   using DynamicVector = Eigen::Matrix<Scalar, -1, 1>;
 
+  TridiagonalSymmetric() = default;
+
   TridiagonalSymmetric(int size)
       : major_diagonal_(DynamicVector(size)),
         side_diagonal_(DynamicVector(size - 1)) {
     assert(size >= 1);
   }
 
-  const DynamicVector& get_major_diagonal() const { return major_diagonal_; }
+  DynamicVector& get_major_diagonal() { return major_diagonal_; }
 
-  const DynamicVector& get_side_diagonal() const { return side_diagonal_; }
+  DynamicVector& get_side_diagonal() { return side_diagonal_; }
 
   void set_diagonals(const DynamicVector& new_major,
                      const DynamicVector& new_side) {
