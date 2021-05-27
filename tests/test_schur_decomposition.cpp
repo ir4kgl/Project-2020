d9253b9ab0453015e331317353d0f276b6b28545
ir4kgl <vishneviyshturman@gmail.com>
2021-05-27 12:06:43 +0300

    update SchurDecomposition tester

--- tests/test_schur_decomposition.cpp
+++ tests/test_schur_decomposition.cpp
@@ -1,8 +1,11 @@
+#include <cstdio>
 #include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <random>
 
 #include "../eigen/Eigen/Dense"
+#include "../eigen/Eigen/Eigenvalues"
 #include "../schur_decomposition/schur_decomposition.h"
 
 namespace test_schur_decomposition {
@@ -14,11 +17,11 @@ using std::min;
 using Algorithm = schur_decomposition::SchurDecomposition<double>;
 using DynamicMatrix = Algorithm::DynamicMatrix;
 
-constexpr const long double input_precision = 1e-10;
-constexpr const long double result_precision = 1e-9;
+constexpr const long double input_precision = 1e-12;
+constexpr const long double result_precision = 1e-11;
 constexpr const int number_of_tests = 100;
 constexpr const int number_of_skipped_tests = 25;
-constexpr const int matrix_size_max = 128;
+constexpr const int matrix_size_max = 100;
 
 void process_triangular_check_failed(const DynamicMatrix& data,
                                      const DynamicMatrix& result, int test_id) {
@@ -96,85 +99,27 @@ bool simple_check(int size, int test_id) {
   DynamicMatrix restored_data = backtrace * result * backtrace.transpose();
   if (!are_indistinguishable(data, restored_data, size)) {
     process_bad_restore(data, restored_data, test_id,
-                        (data - restored_data).norm());
+                        norm(data - restored_data, size));
     return false;
   }
   return true;
 }
 
-unsigned int time_check(int size) {
-  DynamicMatrix data = DynamicMatrix::Random(size, size);
-  DynamicMatrix result;
-  DynamicMatrix backtrace;
-  Algorithm algorithm(input_precision);
-  unsigned int start_time = clock();
-  algorithm.run(data, &result, &backtrace);
-  unsigned int end_time = clock();
-  return end_time - start_time;
-}
-
 void run_stress_testing() {
-  for (int test_id = 1; test_id <= number_of_tests; ++test_id) {
-    for (int size = 1; size <= matrix_size_max; size *= 2) {
+  for (int size = 1; size <= matrix_size_max; ++size) {
+    for (int test_id = 1; test_id <= number_of_tests; ++test_id) {
       srand(test_id);
       if (!simple_check(size, test_id)) return;
-      if (!simple_check(size + 1, test_id)) return;
     }
   }
 
   cout << "Passed SchurDecomposition stress testing\n";
+  cout << "Number of tests: " << number_of_tests << "\n";
+  cout << "Maximum matrix size: " << matrix_size_max << "\n";
   cout << "Input precision: " << input_precision << "\n";
   cout << "Result precision: " << result_precision << "\n\n\n";
 }
 
-void measure_time(int matrix_size) {
-  unsigned long long total_time = 0;
-  double average_time;
-  unsigned int max_time = 0;
-  unsigned int min_time = -1;
-  unsigned long long partial_test_total_time = 0;
-  double partial_test_average_time;
-  unsigned int partial_test_max_time = 0;
-  unsigned int partial_test_min_time = -1;
-
-  for (int test_id = 1; test_id <= number_of_tests; ++test_id) {
-    srand(test_id);
-    unsigned int time_checker = time_check(matrix_size);
-    total_time += time_checker;
-    max_time = max(max_time, time_checker);
-    min_time = min(min_time, time_checker);
-    if (test_id > number_of_skipped_tests) {
-      partial_test_total_time += time_checker;
-      partial_test_max_time = max(partial_test_max_time, time_checker);
-      partial_test_min_time = min(partial_test_min_time, time_checker);
-    }
-  }
-
-  average_time = (double)total_time / number_of_tests;
-  partial_test_average_time = (double)partial_test_total_time /
-                              (number_of_tests - number_of_skipped_tests);
-
-  cout << "SchurDecomposition time measurement results with " << matrix_size
-       << "x" << matrix_size << " matrices\n\n";
-  cout << "Average time: " << average_time / CLOCKS_PER_SEC << "\n";
-  cout << "Partial testing average time : "
-       << partial_test_average_time / CLOCKS_PER_SEC << "\n";
-  cout << "Max time: " << ((double)max_time) / CLOCKS_PER_SEC << "\n";
-  cout << "Partial testing max time : "
-       << ((double)partial_test_max_time) / CLOCKS_PER_SEC << "\n";
-  cout << "Min time: " << ((double)min_time) / CLOCKS_PER_SEC << "\n";
-  cout << "Partial testing min time : "
-       << ((double)partial_test_min_time) / CLOCKS_PER_SEC << "\n";
-  cout << "Number of total tests: " << number_of_tests << "\n";
-  cout << "Number of tests in partial testing: "
-       << number_of_tests - number_of_skipped_tests << "\n";
-}
-
-void run() {
-  cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-  run_stress_testing();
-  measure_time(100);
-  cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
-}
+void run() { run_stress_testing(); }
 
 }  // namespace test_schur_decomposition
