2775367be13784abace0c22952bfba105b860868
ir4kgl <vishneviyshturman@gmail.com>
2021-05-27 12:05:52 +0300

    update SchurDecomposition tester

--- tests/test_schur_decomposition.cpp
+++ tests/test_schur_decomposition.cpp
@@ -7,13 +7,16 @@
 namespace test_schur_decomposition {
 
 using std::cout;
+using std::max;
 using Algorithm = schur_decomposition::SchurDecomposition<double>;
 using DynamicMatrix = Algorithm::DynamicMatrix;
 using DynamicVector = Algorithm::DynamicVector;
 
-constexpr const double precision = 1e-7;
-constexpr const int number_of_tests = 50;
-constexpr const int matrix_size_max = 128;
+constexpr const double input_precision = 1e-12;
+constexpr const double result_precision = 1e-11;
+constexpr const int number_of_tests = 100;
+constexpr const int number_of_skipped_tests = 25;
+constexpr const int matrix_size_max = 100;
 
 void process_unitary_check_failed(const DynamicMatrix& data,
                                   const DynamicMatrix& unitary, int test_id) {
@@ -34,49 +37,59 @@ void process_bad_restore(const DynamicMatrix& old_data,
   cout << "test id: " << test_id << "\n";
 }
 
+bool near_zero(double val) { return abs(val) < result_precision; }
+
+double norm(const DynamicMatrix& data, int size) {
+  double max_element = 0.;
+  for (int i = 0; i < size; ++i) {
+    for (int j = 0; j < size; ++j)
+      max_element = max(abs(data(i, j)), max_element);
+  }
+  return max_element;
+}
+
 bool are_indistinguishable(const DynamicMatrix& first,
                            const DynamicMatrix& second, int size) {
-  return ((first - second).norm() < precision * size * size);
+  return (norm(first - second, size) < result_precision);
 }
 
 bool simple_check(int size, int test_id) {
   DynamicMatrix data = DynamicMatrix::Random(size, size);
   data += (DynamicMatrix)data.transpose();
-
   DynamicVector eigenvalues;
   DynamicMatrix backtrace;
-
-  Algorithm algorithm(precision);
+  Algorithm algorithm(input_precision);
   algorithm.run(data, &eigenvalues, &backtrace);
-
-  if (!backtrace.isUnitary()) {
+  if (!backtrace.isUnitary(result_precision)) {
     process_unitary_check_failed(data, backtrace, test_id);
     return false;
   }
-
   DynamicMatrix restored_data = DynamicMatrix::Zero(size, size);
   restored_data.diagonal(0) = eigenvalues;
   restored_data = backtrace * restored_data * backtrace.transpose();
-
   if (!are_indistinguishable(data, restored_data, size)) {
     process_bad_restore(data, restored_data, test_id,
-                        (data - restored_data).norm());
+                        norm(data - restored_data, size));
     return false;
   }
-
   return true;
 }
 
-void run() {
-  for (int test_id = 1; test_id <= number_of_tests; ++test_id) {
-    for (int size = 2; size <= matrix_size_max; size *= 2) {
+void run_stress_testing() {
+  for (int size = 1; size <= matrix_size_max; ++size) {
+    for (int test_id = 1; test_id <= number_of_tests; ++test_id) {
       srand(test_id);
       if (!simple_check(size, test_id)) return;
-      if (!simple_check(size + 1, test_id)) return;
     }
   }
 
-  cout << "Passed all SchurDecomposition tests\n";
+  cout << "Passed SchurDecomposition stress testing\n";
+  cout << "Number of tests: " << number_of_tests << "\n";
+  cout << "Maximum matrix size: " << matrix_size_max << "\n";
+  cout << "Input precision: " << input_precision << "\n";
+  cout << "Result precision: " << result_precision << "\n\n\n";
 }
+
+void run() { run_stress_testing(); }
 
 }  // namespace test_schur_decomposition
