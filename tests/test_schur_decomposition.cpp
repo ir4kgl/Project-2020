1eb543dea5f982f4d9b407d973e0d4533be5da8b
ir4kgl <vishneviyshturman@gmail.com>
2021-05-29 10:39:19 +0300

    update SchurDecomposition tester

--- tests/test_schur_decomposition.cpp
+++ tests/test_schur_decomposition.cpp
@@ -14,26 +14,29 @@ using DynamicVector = Algorithm::DynamicVector;
 
 constexpr const double input_precision = 1e-12;
 constexpr const double result_precision = 1e-11;
-constexpr const int number_of_tests = 100;
-constexpr const int number_of_skipped_tests = 25;
+constexpr const int number_of_tests = 200;
+constexpr const int number_of_skipped_tests = 50;
 constexpr const int matrix_size_max = 100;
 
 void process_unitary_check_failed(const DynamicMatrix& data,
-                                  const DynamicMatrix& unitary, int test_id) {
+                                  const DynamicMatrix& unitary, int test_id,
+                                  int size) {
   cout << "test failed in SchurDecomposition::run():\n\n";
   cout << "input: M =\n" << data << "\n\n";
   cout << "expected unitary matrix;\n\n";
   cout << "calculated:\n" << unitary << "\n\n";
+  cout << "size:\t" << size << "\n";
   cout << "test id:\t" << test_id << "\n";
 }
 
 void process_bad_restore(const DynamicMatrix& old_data,
                          const DynamicMatrix& restored_data, int test_id,
-                         double delta) {
+                         double delta, int size) {
   cout << "test failed in SchurDecomposition::run(), wrong restore:\n\n";
   cout << "input: M =\n" << old_data << "\n\n";
   cout << "restored: M =\n" << restored_data << "\n\n";
   cout << "delta = " << delta << "\n";
+  cout << "size:\t" << size << "\n";
   cout << "test id: " << test_id << "\n";
 }
 
@@ -61,7 +64,7 @@ bool simple_check(int size, int test_id) {
   Algorithm algorithm(input_precision);
   algorithm.run(data, &eigenvalues, &backtrace);
   if (!backtrace.isUnitary(result_precision)) {
-    process_unitary_check_failed(data, backtrace, test_id);
+    process_unitary_check_failed(data, backtrace, test_id, size);
     return false;
   }
   DynamicMatrix restored_data = DynamicMatrix::Zero(size, size);
@@ -69,7 +72,7 @@ bool simple_check(int size, int test_id) {
   restored_data = backtrace * restored_data * backtrace.transpose();
   if (!are_indistinguishable(data, restored_data, size)) {
     process_bad_restore(data, restored_data, test_id,
-                        norm(data - restored_data, size));
+                        norm(data - restored_data, size), size);
     return false;
   }
   return true;
